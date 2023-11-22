3dae91aa72a90a955ddb80281959a4aa93e189ca
Stuart Marshall <stucmarshall@gmail.com>
2023-11-22 07:49:38 +0000

    Initial Set Up

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import myName from "./myName";
+import printMe from './print.js';
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+
+    element.textContent = myName('Stu');
+    element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+    return element;
+}
+
+document.body.appendChild(component());
