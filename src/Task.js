b956f671d3c507805087a8b146104f9b060873fb
Stuart Marshall <stucmarshall@gmail.com>
2023-11-29 07:46:59 +0000

    create task object

--- src/Task.js
+++ src/Task.js
@@ -1,3 +1,26 @@
-const myName = (name) => 'Hi! My name is ' + name;
+export class task {
+    constructor(id, name, project, description, dueDate, priority) {
+        this.id = id;
+        this.name = name;
+        this.project = project;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
 
-export default myName;
+    setName(name) {
+        this.name = name;
+    }
+
+    setProject(project) {
+        this.project = project;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+}
