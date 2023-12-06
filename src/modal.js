6361f5684df3cc0eeb8f7e92d720950495c76e67
Stuart Marshall <stucmarshall@gmail.com>
2023-12-06 08:13:33 +0000

    style modal

--- src/modal.js
+++ src/modal.js
@@ -5,25 +5,34 @@ export default class Modal {
 
     buildModal() {
         const modal = document.createElement('dialog');
+        modal.classList.add('modal-container');
         const form = document.createElement('form');
         form.method = 'dialog';
+        form.classList.add('modal-form');
         const heading = document.createElement('h1');
         heading.textContent = 'Add a new task';
         const titleInput = document.createElement('input');
         titleInput.type = 'text';
+        titleInput.placeholder = 'Task';
         const descriptionInput = document.createElement('input');
         descriptionInput.type = 'text';
+        descriptionInput.placeholder = 'Description';
         const projectInput = document.createElement('input');
-        projectInput.type = 'dropdown';
+        projectInput.type = 'select';
+        projectInput.placeholder = 'Project';
         const dateInput = document.createElement('input');
         dateInput.type = 'date';
-        const submitButton = document.createElement('button');
-        submitButton.type = 'submit';
-        submitButton.textContent = 'Add';
+        dateInput.placeholder = 'Due Date';
+        const buttons = document.createElement('div');
+        buttons.classList.add('modal-buttons');
         const cancelButton = document.createElement('button');
         cancelButton.type = 'submit';
         cancelButton.textContent = 'Close';
-        form.append(heading, titleInput, descriptionInput, projectInput, dateInput, cancelButton, submitButton);
+        const submitButton = document.createElement('button');
+        submitButton.type = 'submit';
+        submitButton.textContent = 'Add';
+        buttons.append(cancelButton, submitButton,);
+        form.append(heading, titleInput, descriptionInput, projectInput, dateInput, buttons);
         modal.appendChild(form);
 
         return modal;
